3147932e9c3e94b844cc9d0597a900c4665eb075
Danielle <168420921+danielleaop@users.noreply.github.com>
2024-08-19 14:04:33 +0100

    made a flashcard list in flashcards.jsx

--- src/components/flashcards/flashcards.jsx
+++ src/components/flashcards/flashcards.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Flashcard from "./flashcard/flashcard.jsx"
 
 export default function Flashcards(){
+
+    const flashcardList = [{question:"hdwjeowei" ,answer:"hdahiuha"}, {question:"hdwjeowei" ,answer:"hdahiuha"}]
     return (
+        <>
         <div className='flashcardContainer'>
-          
-            <Flashcard />
-            <Flashcard />
-            <Flashcard />
-        
+                {flashcardList.map((flashcard) => (
+      <div className="flashcard-preview" key={flashcard.id}>
+        <h1>🤔</h1>
+        <p>{ flashcard.question }</p>
+        <p>{ flashcard.answer }</p>
         </div>
-    )
+        ))}
+
+  </div>
+   </> )
 }
