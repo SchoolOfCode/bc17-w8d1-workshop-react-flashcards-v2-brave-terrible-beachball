3cbe86ba3ab2328b34c78c3ceaf42c7677c93c0d
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-08-19 13:08:01 +0100

    fixed form submission so that Q&A are showing in console

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -4,33 +4,46 @@ import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 export default function Form(){
-    
-    const [inputs, setInputs] = useState({});
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
 
-  const handleChange = (event) => {
-    const name = event.target.name;
+  const handleQuestion = (event) => {
     const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
+    setQuestion(value)
+  }
+
+  const handleAnswer = (event) => {
+    const value = event.target.value;
+    setAnswer(value)
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(inputs);
+    console.log({ question, answer });
   }
 
     return(
             <>
-            <form>
-                <label for="question">Question:</label>
-                <input type="text" id="question" name="question"></input><br></br>
-
-                <label for="answer">Answer:</label>
-                <input type="text" id="answer" name="answer"></input><br></br>
-
-                <input type="submit" value="Add"></input>
-
-
-
+            <form onSubmit={handleSubmit}>
+            <label>Question:
+                <input 
+                type="text" 
+                name="question" 
+                value={question}
+                onChange={(event) => handleQuestion(event)}
+                />
+                </label>
+                <br></br>
+                <label>Answer:
+                <input 
+                type="text" 
+                name="answer" 
+                value={answer}
+                onChange={(event) => handleAnswer(event)}
+                />
+                </label>
+                <br></br>
+                <button type="submit" value="Add">Add</button>
             </form>
             </>
         )
