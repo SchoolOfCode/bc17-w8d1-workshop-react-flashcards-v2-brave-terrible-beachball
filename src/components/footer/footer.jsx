44fe6c03bf246c32c2587b12ac4e0de8c81ae9a3
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-08-19 10:56:13 +0100

    created basic header and footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./footer.css"
+
+function Footer() {
+    return (
+        <header className="footer">
+            <h1>THIS IS A FOOTER</h1>
+        </header>
+    )
+}
+
+export default Footer
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./header.css"
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>THIS IS A HEADER</h1>
+        </header>
+    )
+}
+
+export default Header
